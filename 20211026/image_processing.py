2969a6870c3d52004a5348ec752d90c2c8930a84
jktmacet <priyo.adi.koesoemo@gmail.com>
2021-10-26 18:17:39 +0700

    Update Day5

--- 20211026/image_processing.py
+++ 20211026/image_processing.py
@@ -12,20 +12,17 @@ print('lebar x panjang foto: %s x %s' %(width, height))
 draw = ImageDraw.Draw(im)  #Prepare canvas for adding text
 text = "KTP ini milik Hariyadi\nDigunakan untuk belajar"  #Assign variable text as string with value "sample water"
 
-font = ImageFont.truetype('Zen_Kurenaido\ZenKurenaido-Regular.ttf', 36)  # Init object text
+font = ImageFont.truetype('arial.ttf', 36)  # Init object text
 
 textwidth, textheight = draw.textsize(text, font)
 
 # calculate the x,y coordinates of the text
 margin = 10
-x = width//2 - textwidth - margin
-y = height//2 - textheight - margin
-
-x = width//2 - textwidth //2
-y = height//2 - textheight  //2
+x = width - textwidth - margin
+y = height - textheight - margin
 
 # draw watermark in the bottom right corner
-draw.text((x, y), text, font=font, fill=(255,255,255,60)) #RGBA 0-255
+draw.text((x, y), text, font=font, fill=(255,0,0,60)) #RGBA 0-255
 im.show()
 
 # #Save watermarked image
